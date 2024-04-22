42cfdbbbf25becf5460c922b63d582a42d01f644
snoopy910 <snoopy5473cool@gmail.com>
2024-04-22 22:38:27 +0900

    feat: add infinite scrolling

--- src/contexts/documents.context.tsx
+++ src/contexts/documents.context.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
 import { DEFAULT_DOCUMENTS, IDocument } from "../consts/documents";
+import { useFetch } from "../hooks/useFetch";
 
 interface DocumentsContextProps {
   children: React.ReactNode;
@@ -7,11 +8,15 @@ interface DocumentsContextProps {
 
 interface DocumentsContextType {
   documents: IDocument[];
+  loading: boolean;
+  fetchDocuments: (id: number) => void;
   addLabels: (ID: number, labels: string[] | undefined) => void;
 }
 
 const defaultDocuments: DocumentsContextType = {
   documents: [],
+  loading: false,
+  fetchDocuments: () => {},
   addLabels: () => {},
 };
 
@@ -27,12 +32,24 @@ export const DocumentsContextProvider: React.FC<DocumentsContextProps> = ({
         return JSON.parse(storedDocuments);
       } catch (error) {
         console.error("Error parsing documents from LocalStorage", error);
-        return DEFAULT_DOCUMENTS;
+        return DEFAULT_DOCUMENTS.slice(0, 20);
       }
     } else {
-      return DEFAULT_DOCUMENTS;
+      return DEFAULT_DOCUMENTS.slice(0, 20);
     }
   });
+
+  const [fetchDocuments, { data, loading }] = useFetch();
+
+  useEffect(() => {
+    localStorage.setItem("documents", JSON.stringify(documents));
+  }, [documents]);
+
+  useEffect(() => {
+    if (!loading && data) {
+      setDocuments((prevDocuments) => [...prevDocuments, ...data]);
+    }
+  }, [data, loading]);
 
   const addLabels = (ID: number, newLabels: string[] | undefined) => {
     const updatedDocuments = documents.map((doc, index) => {
@@ -44,14 +61,12 @@ export const DocumentsContextProvider: React.FC<DocumentsContextProps> = ({
     setDocuments(updatedDocuments);
   };
 
-  useEffect(() => {
-    localStorage.setItem("documents", JSON.stringify(documents));
-  }, [documents]);
-
   return (
     <DocumentsContext.Provider
       value={{
         documents,
+        loading,
+        fetchDocuments,
         addLabels,
       }}
     >
